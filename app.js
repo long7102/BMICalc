6b8448d02f3ddfe3e761683ebd18c926e4369d8e
Nguyễn Việt Long <105055082+long7102@users.noreply.github.com>
2024-10-21 09:45:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function calculateBMI(e) {
     maxWeight.innerText = `${max}kgs`;
 
     if (bmi < 18.5) {
-      classification.innerText = 'Thừa cân';
+      classification.innerText = 'Gầy';
     } else if (bmi >= 18.5 && bmi <= 24.9) {
       classification.innerText = 'Cân đối';
     } else if (bmi > 24.9 && bmi <= 29.9) {
@@ -94,13 +94,13 @@ function calculateBMI(e) {
     maxWeight.innerText = `${maxWeightInStone}st ${maxWeightInPounds}lbs`
 
     if (bmi < 18.5) {
-      classification.innerText = 'Underweight';
+      classification.innerText = 'Gầy';
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-      classification.innerText = 'Healthy weight';
+      classification.innerText = 'Cân đối';
     } else if (bmi > 24.9 && bmi <= 29.9) {
-      classification.innerText = 'Overweight';
+      classification.innerText = 'Thừa cân';
     } else {
-      classification.innerText = 'Obese';
+      classification.innerText = 'Béo phì';
     }
   }
 }
